7e75dbba81887e37d73fa02771e8c4debca2806a
Andrey Chernih <andrey.chernyh@flatsoft.com>
2009-10-20 20:06:27 +0400

    User of Shoulda and Factory Girl

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,46 +1,28 @@
 require 'spec_helper'
-
-$valid_attributes = {
-	:city => 'Kazan',
-	:address1 => 'Gavrilova',
-	:zip => '420133',
-	:social_security_number => '123456',
-	:birth_date => '11/11/1985',
-	:sex => 'M',
-	:phone => '111-11-11',
-	:first_name => 'Andrey',
-	:last_name => 'Chernih',
-	:state => 'Russia',
-	:email => 'andrey.cherniH@gmail.com'
-}
-
+require 'factories'
 
 describe Patient do
   before(:each) do
-		@valid_attributes = $valid_attributes
+		@patient = Factory.create(:patient)
   end
+
+	it { should validate_presence_of :first_name }
+	it { should validate_presence_of :last_name }
+	it { should validate_presence_of :email }
+	it { should validate_presence_of :address1 }
+	it { should validate_presence_of :city }
+	it { should validate_presence_of :state }
+	it { should validate_presence_of :zip }
+	it { should validate_presence_of :phone }
+	it { should validate_presence_of :social_security_number }
+	it { should validate_presence_of :birth_date }
+	it { should validate_presence_of :sex }
 
   it "should create a new instance given valid attributes" do
-    Patient.create!(@valid_attributes)
+    Factory.create(:patient)
   end
 
-	def self.should_not_be_valid_if
-		$valid_attributes.keys.each do |attribute|
-			it "should not be valid if not specified required attrubute #{attribute}" do
-				@current_attributes = @valid_attributes.clone
-				@current_attributes[attribute] = nil
-
-				@patient = Patient.new @current_attributes
-				@patient.should_not be_valid
-				@patient.errors.on(attribute).should eql('can\'t be blank')
-			end
-		end
-	end
-
-	should_not_be_valid_if
-
 	it 'should be valid with a full set of valid attributes' do
-		@patient = Patient.new @valid_attributes
 		@patient.should be_valid
 	end
 end
