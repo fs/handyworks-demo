8a8b27e1945aa12e7764a3b2beaf18f55f1e0198
Andrey Chernih <andrey.chernyh@flatsoft.com>
2009-10-21 19:00:49 +0400

    Marked required fields

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,13 +6,13 @@ class Patient < ActiveRecord::Base
 	validates_numericality_of :account
 
 	ZIP_FORMAT = /^\d{5}(-\d{4})?$/
-	ZIP_MESSAGE = 'should be 12345 or 12345-1234'
+	ZIP_MESSAGE = 'should be in the following format: XXXXX or XXXXX-XXXX'
 
 	PHONE_FORMAT = /^\d{3}-\d{2}-\d{2}$/
-	PHONE_MESSAGE = 'should be 123-45-67'
+	PHONE_MESSAGE = 'should be in the following format: XXX-XX-XX'
 
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-	validates_format_of :social_security_number, :with => /^\d{3}[-]?\d{2}[-]?\d{4}$/i, :message => 'should be 123-45-6789'
+	validates_format_of :social_security_number, :with => /^\d{3}[-]?\d{2}[-]?\d{4}$/i, :message => 'should be in the following format: XXX-XX-XXXX'
 	validates_format_of :zip, :with => ZIP_FORMAT, :message => ZIP_MESSAGE
 	validates_format_of :phone, :with => PHONE_FORMAT, :message => PHONE_MESSAGE
 
