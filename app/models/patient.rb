b43231a4a992b19bd0a28c7906065060fc951dfd
Andrey Chernih <andrey.chernyh@flatsoft.com>
2009-10-21 17:27:16 +0400

    Validation and layout changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,21 @@
 class Patient < ActiveRecord::Base
 	validates_presence_of :first_name, :last_name, :email
 	validates_presence_of :social_security_number, :birth_date, :sex
-	validates_presence_of :address1, :city, :state, :zip, :phone
+	validates_presence_of :address1, :city, :state, :zip, :phone, :account
+
+	validates_numericality_of :account
+
+	ZIP_FORMAT = /^\d{5}(-\d{4})?$/
+	ZIP_MESSAGE = 'should be 12345 or 12345-1234'
+
+	PHONE_FORMAT = /^\d{3}-\d{2}-\d{2}$/
+	PHONE_MESSAGE = 'should be 123-45-67'
+
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+	validates_format_of :social_security_number, :with => /^\d{3}[-]?\d{2}[-]?\d{4}$/i, :message => 'should be 123-45-6789'
+	validates_format_of :zip, :with => ZIP_FORMAT, :message => ZIP_MESSAGE
+	validates_format_of :phone, :with => PHONE_FORMAT, :message => PHONE_MESSAGE
+
+	validates_format_of :work_zip, :with => ZIP_FORMAT, :message => ZIP_MESSAGE, :allow_nil => true, :allow_blank => true
+	validates_format_of [ :work_phone, :cell_phone ], :with => PHONE_FORMAT, :message => PHONE_MESSAGE, :allow_nil => true, :allow_blank => true
 end
