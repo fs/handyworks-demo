f30362db48e321fe96f58e8c3cca6d2016d78c41
Anton Rogov <anton.s.rogov@gmail.com>
2009-10-22 19:50:47 +0400

    cleaning up

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091020073028) do
+ActiveRecord::Schema.define(:version => 20091020070108) do
 
   create_table "patients", :force => true do |t|
     t.string   "title"
